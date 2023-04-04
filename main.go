369e2f77e3886b0ac142ddcb1325858d3050db6e
maru <maru@myyahoo.com>
2023-04-04 09:35:24 -0400

    Add missing call to flag.Parse in main

--- main.go
+++ main.go
@@ -27,6 +27,8 @@ import (
 func main() {
 	address := flag.String("address", "0.0.0.0", "address for the server to listen on")
 	port := flag.Int("port", 8080, "port for the server to listen on")
+	
+	flag.Parse()
 
 	err := api.Init(address, port)
 	if err != nil {
